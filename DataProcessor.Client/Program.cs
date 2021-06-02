a8a06de1bb4c4a8c383fcd43be8b5d8c78ca6275
Vivek Mehta <vmehta.108@gmail.com>
2021-06-02 15:20:14 +0100

    Add project files.

---
+++ DataProcessor.Client/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DataProcessor.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data Extractor 1.0");
+            Console.Read();
+        }
+    }
+}
