a36398a64c1e47fa8d5cf26819f6cba356f29d44
Vivek Mehta <vmehta.108@gmail.com>
2021-06-02 17:20:08 +0100

    Completed first test.

--- DataProcessor.Service/OutputRecord.cs
+++ DataProcessor.Service/OutputRecord.cs
@@ -2,6 +2,16 @@
 {
     public class OutputRecord
     {
+        [CsvHelper.Configuration.Attributes.Name("ISIN")] 
+        public string Isin { get; set; }
         
+        [CsvHelper.Configuration.Attributes.Name("CFICode")] 
+        public string CfiCode { get; set; }
+        
+        [CsvHelper.Configuration.Attributes.Name("Venue")] 
+        public string Venue { get; set; }
+        
+        [CsvHelper.Configuration.Attributes.Name("Contract Size")] 
+        public string AlgoParam { get; set; }   
     }
 }
