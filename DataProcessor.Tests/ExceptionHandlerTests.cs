759bc4066b660bdbe49e5dba8bc45dfcd6a934f0
Vivek Mehta <vmehta.108@gmail.com>
2021-06-02 17:25:57 +0100

    Added ExceptionHandler tests. Should fail at first.

---
+++ DataProcessor.Tests/ExceptionHandlerTests.cs
@@ -0,0 +1,20 @@
+﻿using DataProcessor.Service;
+using NUnit.Framework;
+
+namespace DataProcessor.Tests
+{
+    [TestFixture]
+    public class ExceptionHandlerTests
+    {
+        [Test]
+        public void ExceptionHandler_Should_Implement_IExceptionHandler()
+        {
+            ExceptionHandler exceptionHandler = new ExceptionHandler();
+
+            var testInterface =  exceptionHandler as IExceptionHandler;
+
+            Assert.IsNotNull(testInterface);
+
+        }
+    }
+}
