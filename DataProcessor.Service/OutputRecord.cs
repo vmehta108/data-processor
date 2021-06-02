b4f841930e683eda90186a4ee4f5cf94d5466765
Vivek Mehta <vmehta.108@gmail.com>
2021-06-02 17:17:45 +0100

    Added first test. Following TDD.

---
+++ DataProcessor.Service/OutputRecord.cs
@@ -0,0 +1,7 @@
+﻿namespace DataProcessor.Service
+{
+    public class OutputRecord
+    {
+        
+    }
+}
---
+++ DataProcessor.Tests/OutputRecordTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Reflection;
+using DataProcessor.Service;
+using NUnit.Framework;
+
+namespace DataProcessor.Tests
+{
+    [TestFixture]
+    public class OutputRecordTests
+    {
+        [Test]
+        public void OutputRecord_Should_Have_Necessary_Properties()
+        {
+            /* Arrange */
+            OutputRecord outputRecord = new OutputRecord();
+
+            /* Act */
+            PropertyInfo[] propertyInfos = outputRecord.GetType().GetProperties();
+            
+            /* Assert */
+            Assert.AreEqual(4, propertyInfos.Length);
+            Assert.IsTrue(propertyInfos.Any(p => p.Name == "Isin"));
+            Assert.IsTrue(propertyInfos.Any(p => p.Name == "CfiCode"));
+            Assert.IsTrue(propertyInfos.Any(p => p.Name == "Venue"));
+            Assert.IsTrue(propertyInfos.Any(p => p.Name == "AlgoParam"));
+        }
+    }
+}
